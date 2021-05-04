f83b65d40d51942d7d799520144030a543cd6bc9
Finn Andersen <Finn.Andersen@team.telstra.com>
2021-05-04 17:17:33 +1000

    - Tweaks to DataframetoCSVExporter

--- etl_framework/operations/pandas/output_generators/file.py
+++ etl_framework/operations/pandas/output_generators/file.py
@@ -13,22 +13,18 @@ class DataFrameToCSVExporter(BaseDataframeExporter):
 
     calling_translations = {'dataframe': 'file_data'}
 
-    def __init__(self, columns=None, delimiter=',', header=True, line_terminator='\n', float_format=None,
-                 writer_kwargs=None, **kwargs):
+    def __init__(self, columns=None, delimiter=',', line_terminator='\n', create_for_empty=False, **to_csv_kwargs):
         """
         :param sequence columns: sequence of field names to write. Will use all if not specified
         :param str delimiter: field delimiter character
-        :param bool/list of str header: Whether to write field names, or aliases for field names
         :param str line_terminator: line terminator string
-        :param str float_format: Optional formatting string for float values
-        :param dict writer_kwargs: extra arguments to provide to pandas.to_csv()
+        :param bool create_for_empty: Whether to create output file when dataframe is empty
+        :param to_csv_kwargs: extra arguments to provide to pandas.to_csv()
         """
-        self.header = header
         self.delimiter = delimiter
         self.line_terminator = line_terminator
-        self.float_format = float_format
-        self.writer_kwargs = writer_kwargs or {}
-        super().__init__(columns=columns, **kwargs)
+        self.writer_kwargs = to_csv_kwargs
+        super().__init__(columns=columns, create_for_empty=create_for_empty)
 
     def generate_output(self, dataframe):
         buffer = io.StringIO()
@@ -36,10 +32,8 @@ class DataFrameToCSVExporter(BaseDataframeExporter):
         dataframe.to_csv(buffer,
                          sep=self.delimiter,
                          columns=self.columns,
-                         header=self.header,
                          index=False,
                          line_terminator=self.line_terminator,
-                         float_format=self.float_format,
                          **self.writer_kwargs)
         return buffer.getvalue()
 
